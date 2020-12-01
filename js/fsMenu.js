d4754e367d34d1d0a4474c9894c2d15f095d5bc9
Roy Macdonald <macdonald.roy@gmail.com>
2020-12-01 13:37:45 -0300

    Trying to fix localstorage persistance

--- js/fsMenu.js
+++ js/fsMenu.js
@@ -6,7 +6,11 @@ function enableFutureSketchesMenu()
         	fsMenu.style = "";
             fsMenu.classList.add("current")
         }
-        localStorage.showFutureSketches =  true;	
+
+
+        window.localStorage.setItem('showFutureSketches', 'true');	
+        
+
         let nav = document.querySelector(".main-nav");
         if(nav){
             let current = nav.querySelector(".current");
@@ -18,19 +22,22 @@ function enableFutureSketchesMenu()
         
 
 }
-document.addEventListener("DOMContentLoaded", function(event) {
-
-	if(window.location.pathname == "/future_sketches/")
+// document.addEventListener("DOMContentLoaded", function(event) {
+window.onload = function() {
+    console.log(window.localStorage);
+	if(window.location.pathname.includes("future_sketches"))
 	{
-		
+        console.log("future_sketches");		
 		enableFutureSketchesMenu();
 	}else{
-		let bShowFutureSketches = localStorage.showFutureSketches;
-    	
+		let bShowFutureSketches = window.localStorage.getItem('showFutureSketches');
+    	console.log(bShowFutureSketches);
     	if (bShowFutureSketches != null && (bShowFutureSketches == "true" || bShowFutureSketches == true)){
+            console.log("nojsdfak sm");
     		enableFutureSketchesMenu();
     	}	
 	}
 
 
-});
+}
+//);
